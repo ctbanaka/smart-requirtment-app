979ef1b184b1661c209e22498a160ece64318641
Vinod Dhirde <atharv.vinod-dhirde@capgemini.com>
2023-01-05 17:59:28 +0530

    service update

--- src/app/services/offerspending.service.ts
+++ src/app/services/offerspending.service.ts
@@ -6,16 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class OfferspendingService {
-/*
-  private baseURL = "http://localhost:8083/reservation";
-  constructor(private httpClient: HttpClient) { }
 
-  getReservationList(): Observable<any> {
-
-    return this.httpClient.get<any>( "http://localhost:8083/reservation/ShowAllReservations");
-
-  }*/
-  private baseURL = "http://192.168.1.9:3300/users";
+  private baseURL = "http://192.168.1.8:3300/users";
   constructor(private httpClient: HttpClient) { }
 
   getOffersPendingList(): Observable<any> {
