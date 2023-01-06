4d1610c38c39a079ae309bd03a823339d81d5d4b
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2023-01-06 17:43:02 +0530

    changes

--- app/models/index.js
+++ app/models/index.js
@@ -24,21 +24,21 @@ db.gradeMaster = require("./grade_master.model.js")(sequelize, Sequelize);
 db.candidate = require("./candidate.model.js")(sequelize, Sequelize);
 db.accountMaster= require("./account_master.model.js")(sequelize, Sequelize);
 db.skillMaster = require("./skill_master.model.js")(sequelize, Sequelize);
-db.candidateFeedbackInfo= require("./candidate_feedback_info.model.js")
+db.candidateFeedbackInfo= require("./candidate_feedback_info.model.js")(sequelize, Sequelize);
 db.candidateExperience = require("./candidate_experience.model.js")(sequelize, Sequelize);
 db.candidateSelect = require("./candidate_select.model.js")(sequelize, Sequelize);
 db.candidateReject = require("./candidate_reject.model.js")(sequelize, Sequelize);
 db.skillMaster= require("./skill_master.model.js")(sequelize, Sequelize);
 db.candidateCTC= require("./candidate_CTC.model.js")(sequelize, Sequelize);
-db.candidateJoiningDetails= require("./candidate_joining_details.model.js")
+db.candidateJoiningDetails= require("./candidate_joining_details.model.js")(sequelize, Sequelize);
 
 
 db.candidateDetails.belongsTo(db.skillMaster,{foriegnKey:'skill_id'});
 db.candidateDetails.belongsTo(db.gradeMaster,{foriegnKey:'geade_id'});
 db.candidateDetails.belongsTo(db.accountMaster,{foriegnKey:'account_id'})
-db.candidateExperience.belongsTo(db.candidateJoiningDetails);
+db.candidateExperience.belongsTo(db.candidateDetails);
 db.candidateCTC.belongsTo(db.candidateDetails);
-db.candidateFeedbackInfo.belongsTo(db.candidateDetails);
+db.candidateFeedbackInfo.belongsTo(db.candidateDetails,{foriegnKey:'candidate_Id'});
 db.candidateSelect.belongsTo(db.candidateDetails);
 db.candidateReject.belongsTo(db.candidateDetails);
 
