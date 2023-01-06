7efd08ee3b6dc0db255231ebeeaa370df3f834c2
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2023-01-06 16:42:07 +0530

    pk fk

--- app/models/index.js
+++ app/models/index.js
@@ -19,7 +19,19 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.candidateDetails= require("./candidate_details.model.js")(sequelize, Sequelize);
+db.gradeMaster = require("./grade_master.model.js")(sequelize, Sequelize);
 db.candidate = require("./candidate.model.js")(sequelize, Sequelize);
+db.accountMaster= require("./account_master.model.js")(sequelize, Sequelize);
 db.skillMaster = require("./skill_master.model.js")(sequelize, Sequelize);
 db.candidateExperience = require("./candidate_experience.model.js")(sequelize, Sequelize);
+db.skillMaster= require("./skill_master.model.js")(sequelize, Sequelize);
+
+
+
+db.candidateDetails.belongsTo(db.skillMaster,{foriegnKey:'skill_id'});
+db.candidateDetails.belongsTo(db.gradeMaster,{foriegnKey:'geade_id'});
+db.candidateDetails.belongsTo(db.accountMaster,{foriegnKey:'account_id'})
+
+
 module.exports = db;
