3e8cc4fe0faad05b9baa3c89d7267b6dab1d27dc
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2023-01-06 11:32:39 +0530

    update

---
+++ app/models/candidate_feedback_info.model.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, Sequelize) =>{
+ 
+    const candidateFeedback = sequelize.define("candidate_feedback_info",{
+
+        candidate_ID:{
+            type: Sequelize.INTEGER  
+        },
+
+        l1_panel_name:{
+            type:Sequelize.STRING
+        
+        },
+
+        l1_avarage_rating:{
+            type:Sequelize.STRING
+        },
+        
+        l2_panel_name:{
+            type:Sequelize.STRING
+        
+        },
+
+        l2_avarage_rating:{
+            type:Sequelize.STRING
+        },
+         
+        created_by:{
+            type:Sequelize.STRING
+        },
+        
+        updated_by:{
+            type:Sequelize.STRING
+        }
+
+    });
+    
+    return candidateFeedback;
+}
